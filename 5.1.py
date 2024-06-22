fa030cb8b7517bf385af7f30f07e64e2aa8c14b3
Emil Mustafin <63415153+EmilMustafin@users.noreply.github.com>
2024-06-22 19:00:42 +0300

    Update 5.1.py

--- 5.1.py
+++ 5.1.py
@@ -1,57 +1,59 @@
 import turtle
 import math
 
+def draw_square(side_length):
+    for _ in range(4):
+        turtle.forward(side_length)
+        turtle.right(90)
+
+def draw_triangle(side_length):
+    for _ in range(3):
+        turtle.forward(side_length)
+        turtle.right(120)
+
+def draw_figure(side_length):
+    draw_square(side_length)
+    turtle.right(90)
+    turtle.forward(side_length - (side_length * math.sqrt(3)) / 2)
+    turtle.left(90)
+    draw_triangle(side_length)
+    turtle.forward(side_length / 2)
+    turtle.right(180)
+    radius = ((side_length ** 2) * math.sqrt(3)) / (4 * side_length * 3 / 2)
+    turtle.circle(radius)
+    return radius
+
+def move_to_next_figure(radius):
+    turtle.pencolor('white')
+    turtle.left(90)
+    turtle.forward(radius)
+    turtle.right(135)
+    turtle.forward(radius)
+    turtle.right(135)
+    turtle.pencolor('black')
+
 square_side = int(input("сторона квадрата: "))
 n = int(input("количество вложений: "))
+
+
 turtle.hideturtle()
 turtle.pencolor('white')
 turtle.left(90)
-turtle.forward(square_side/2)
+turtle.forward(square_side / 2)
 turtle.left(90)
-turtle.forward(square_side/2)
+turtle.forward(square_side / 2)
 turtle.left(180)
 turtle.pencolor('black')
 
-turtle.speed(speed=9)
-turtle.shape("turtle")
-for i in range(4):
-    turtle.forward(square_side)
-    turtle.right(90)
-turtle.right(90)
-turtle.forward(square_side - ((square_side * math.sqrt(3))/2))
-turtle.left(90)
-for _ in range(3):
-    turtle.forward(square_side)
-    turtle.right(120)
-turtle.forward(square_side / 2)
-turtle.right(180)
-radius = ((square_side * square_side * math.sqrt(3))/ 4) / ((square_side * 3)/2)
-turtle.circle(radius)
 
-if n > 1 and n < 5:
+radius = draw_figure(square_side)
+
+
+if 1 < n < 5:
     for _ in range(n - 1):
-        turtle.pencolor('white')
-        turtle.left(90)
-        turtle.forward(radius)
-        turtle.right(90+45)
-        turtle.forward(radius)
-        turtle.right(90+45)
-        turtle.pencolor('black')
+        move_to_next_figure(radius)
         square_side = math.sqrt(2) * radius
-        for i in range(4):
-            turtle.forward(square_side)
-            turtle.right(90)
-        turtle.right(90)
-        turtle.forward(square_side - ((square_side * math.sqrt(3))/2))
-        turtle.left(90)
-        for _ in range(3):
-            turtle.forward(square_side)
-            turtle.right(120)
-        turtle.forward(square_side / 2)
-        turtle.right(180)
-        radius = ((square_side * square_side * math.sqrt(3))/ 4) / ((square_side * 3)/2)
-        turtle.circle(radius)
+        radius = draw_figure(square_side)
 
 
 turtle.exitonclick()
-
